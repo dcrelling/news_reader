367e041c8ad18dfe31b64868a16a8f2ac4104c10
dcrelling <david_crelling@hotmail.com>
2016-08-13 19:02:41 -0400

    add a custom arrayadaptor also some refactoring

--- app/src/main/java/com/example/dcrelling/newsreader/adaptor/ArticleAdapter.java
+++ app/src/main/java/com/example/dcrelling/newsreader/adaptor/ArticleAdapter.java
@@ -4,9 +4,12 @@ import java.util.List;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import com.example.dcrelling.newsreader.R;
 import com.example.dcrelling.newsreader.service.response.ArticleSearchResponse;
 
 /**
@@ -25,6 +28,14 @@ public class ArticleAdapter extends ArrayAdapter<ArticleSearchResponse.Article>
   @Override
   public View getView(int position, View convertView, ViewGroup parent)
   {
-    return super.getView(position, convertView, parent);
+    ArticleSearchResponse.Article article = getItem(position);
+    if (convertView == null)
+    {
+      convertView = LayoutInflater.from(getContext()).inflate(R.layout.article_list_item, parent, false);
+    }
+    TextView headline = (TextView) convertView.findViewById(R.id.headline);
+    headline.setText(article.getHeadline().getMain());
+    return convertView;
+
   }
 }
